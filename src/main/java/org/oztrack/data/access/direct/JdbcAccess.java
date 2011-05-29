7d347a159fae4bac54d22978cd6418898a996689
Peggy Newman <peggydnewman@gmail.com>
2011-05-29 23:17:41 +0000

    all projects listing

--- src/main/java/org/oztrack/data/access/direct/JdbcAccess.java
+++ src/main/java/org/oztrack/data/access/direct/JdbcAccess.java
@@ -1,17 +1,23 @@
 package org.oztrack.data.access.direct;
 
+import org.oztrack.data.model.AcousticDetection;
+import org.oztrack.data.model.SearchQuery;
+
+import java.util.List;
+
 /**
  * Created by IntelliJ IDEA.
  * User: uqpnewm5
  * Date: 9/05/11
  * Time: 12:12 PM
- * To change this template use File | Settings | File Templates.
  */
 public interface JdbcAccess {
 
-    public void testJDBCAccess();
+    //public void testJDBCAccess();
 
     public int loadAcousticDetections(Long projectId, Long dataFileId);
     public void truncateRawAcousticDetections();
+    public List<AcousticDetection> queryAcousticDetections(String sql);
+    public List<AcousticDetection> queryAcousticDetections2(SearchQuery searchQuery);
 
-    }
+}
