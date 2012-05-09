2d885413f04d6c311225f38d13c2316a709a58d0
Charles Brooking <c.brooking@uq.edu.au>
2012-05-09 14:30:21 +1000

    removed useless projectList model object in home

--- src/main/java/org/oztrack/controller/HomeController.java
+++ src/main/java/org/oztrack/controller/HomeController.java
@@ -1,16 +1,11 @@
 package org.oztrack.controller;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.oztrack.app.Constants;
-import org.oztrack.app.OzTrackApplication;
-import org.oztrack.data.access.ProjectDao;
-import org.oztrack.data.model.Project;
 import org.oztrack.data.model.User;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
@@ -21,12 +16,9 @@ public class HomeController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
         User currentUser = (User) httpServletRequest.getSession().getAttribute(Constants.CURRENT_USER);
-        ProjectDao projectDao = OzTrackApplication.getApplicationContext().getDaoManager().getProjectDao();
-        List<Project> projectList = projectDao.getAll();
 
         ModelAndView modelAndView = new ModelAndView("home");
         modelAndView.addObject(Constants.CURRENT_USER, currentUser);
-        modelAndView.addObject("projectList", projectList);
         return modelAndView;
     }
 }
