72c276a354b0b3f62447e8230265e4b17490e438
Charles Brooking <c.brooking@uq.edu.au>
2012-05-09 14:23:38 +1000

    removed weird reuse of HomeCOntroller for about and contact pages

--- src/main/java/org/oztrack/controller/AboutController.java
+++ src/main/java/org/oztrack/controller/AboutController.java
@@ -1,32 +1,24 @@
 package org.oztrack.controller;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.oztrack.app.Constants;
-import org.oztrack.app.OzTrackApplication;
-import org.oztrack.data.access.ProjectDao;
-import org.oztrack.data.model.Project;
 import org.oztrack.data.model.User;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-public class HomeController implements Controller {
+public class AboutController implements Controller {
     protected final Log logger = LogFactory.getLog(getClass());
 
     @Override
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
         User currentUser = (User) httpServletRequest.getSession().getAttribute(Constants.CURRENT_USER);
-        ProjectDao projectDao = OzTrackApplication.getApplicationContext().getDaoManager().getProjectDao();
-        List<Project> projectList = projectDao.getAll();
 
-        ModelAndView modelAndView = new ModelAndView("home");
+        ModelAndView modelAndView = new ModelAndView("about");
         modelAndView.addObject(Constants.CURRENT_USER, currentUser);
-        modelAndView.addObject("projectList", projectList);
         return modelAndView;
     }
 }
