78637d82ccee1809dab8e22f3c04456696a19b40
Charles Brooking <c.brooking@uq.edu.au>
2012-05-14 16:02:45 +1000

    use mvn:annotation-driven; apply to / resource

--- src/main/java/org/oztrack/controller/HomeController.java
+++ src/main/java/org/oztrack/controller/HomeController.java
@@ -1,24 +1,21 @@
 package org.oztrack.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.oztrack.app.Constants;
 import org.oztrack.data.model.User;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-public class HomeController implements Controller {
-    protected final Log logger = LogFactory.getLog(getClass());
-
-    @Override
-    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-        User currentUser = (User) httpServletRequest.getSession().getAttribute(Constants.CURRENT_USER);
-
-        ModelAndView modelAndView = new ModelAndView("home");
-        modelAndView.addObject(Constants.CURRENT_USER, currentUser);
-        return modelAndView;
+@Controller
+@RequestMapping(value={"/","/home"})
+public class HomeController {
+    @RequestMapping(method=RequestMethod.GET)
+    public String getView(HttpSession session, Model model) {
+        User currentUser = (User) session.getAttribute(Constants.CURRENT_USER);
+        model.addAttribute(Constants.CURRENT_USER, currentUser);
+        return "home";
     }
 }
