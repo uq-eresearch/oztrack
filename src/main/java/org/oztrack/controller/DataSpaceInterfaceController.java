bb60d93cd8fbe9d2f280896983662dd53be69792
Charles Brooking <c.brooking@uq.edu.au>
2012-05-16 11:24:58 +1000

    dataspace controller to annotations

--- src/main/java/org/oztrack/controller/DataSpaceInterfaceController.java
+++ src/main/java/org/oztrack/controller/DataSpaceInterfaceController.java
@@ -3,9 +3,6 @@ package org.oztrack.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.oztrack.app.OzTrackApplication;
@@ -13,28 +10,33 @@ import org.oztrack.data.access.ProjectDao;
 import org.oztrack.data.access.UserDao;
 import org.oztrack.data.model.Project;
 import org.oztrack.data.model.User;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-public class DataSpaceInterfaceController implements Controller {
+@Controller 
+public class DataSpaceInterfaceController {
     protected final Log logger = LogFactory.getLog(getClass());
     
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        String projectId = request.getParameter("project");
-        String username = request.getParameter("username");
-        String action = request.getParameter("action");
-        
+    @RequestMapping(value="/dataspace", method=RequestMethod.POST)
+	public String handleRequest(
+	    Model model,
+	    @RequestParam(value="project", required=false) String projectId,
+	    @RequestParam(value="username", required=false) String username,
+	    @RequestParam(value="action", required=false) String action
+    ) throws Exception {
         UserDao userDao = OzTrackApplication.getApplicationContext().getDaoManager().getUserDao();
         User currentUser = userDao.getByUsername(username);
         ProjectDao projectDao = OzTrackApplication.getApplicationContext().getDaoManager().getProjectDao();
 
         if (currentUser == null) {
-        	return new ModelAndView("login");
+        	return "login";
         }
         else {
         	if (projectId == null) {
-        		return new ModelAndView("projects");
+        		return "projects";
         	}
         	else {
         		Project project = projectDao.getProjectById(Long.parseLong(projectId));
@@ -43,7 +45,9 @@ public class DataSpaceInterfaceController implements Controller {
         		projectActionMap.put("action", action);
         		
         		logger.info("request for dataspace syndication by user: " + currentUser.getUsername() + " for project: " + project.getTitle());
-        		return new ModelAndView("java_DataSpaceInterface", "projectActionMap", projectActionMap);
+        		
+        		model.addAttribute("projectActionMap", projectActionMap);
+        		return "java_DataSpaceInterface";
         	}
         }
 	}
