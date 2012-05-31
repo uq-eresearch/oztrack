7ce758c83880c4c579a094111658dd0ac237ec04
Charles Brooking <c.brooking@uq.edu.au>
2012-05-31 15:32:08 +1000

    removed restlet dependencies

--- src/main/java/org/oztrack/data/connector/JpaConnector.java
+++ src/main/java/org/oztrack/data/connector/JpaConnector.java
@@ -5,17 +5,9 @@ import java.io.Serializable;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
-import org.restlet.service.ConnectorService;
-
 import au.edu.uq.itee.maenad.dataaccess.jpa.EntityManagerSource;
 
-/**
- * Author: alabri
- * Date: 9/03/11
- * Time: 11:10 AM
- */
-public class JpaConnector extends ConnectorService implements EntityManagerSource, Serializable {
-
+public class JpaConnector implements EntityManagerSource, Serializable {
     private final EntityManagerFactory emf;
     private final ThreadLocal<EntityManager> entityManagerTL = new ThreadLocal<EntityManager>();
 
@@ -32,5 +24,4 @@ public class JpaConnector extends ConnectorService implements EntityManagerSourc
         }
         return entityManager;
     }
-
 }
