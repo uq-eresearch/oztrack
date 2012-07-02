dfb189ceb67ff6328111b9099c38624142533c1e
Charles Brooking <c.brooking@uq.edu.au>
2012-07-02 13:35:26 +1000

    remove dead code: animal sightings

--- src/main/java/org/oztrack/data/access/SightingDao.java
+++
@@ -1,9 +0,0 @@
-package org.oztrack.data.access;
-
-import org.oztrack.data.model.Sighting;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface SightingDao {
-    void save(Sighting sighting);
-}
