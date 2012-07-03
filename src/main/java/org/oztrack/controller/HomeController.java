f2bcf6a6df946c6194dd6b0f5bffa31eb5485e24
Charles Brooking <c.brooking@uq.edu.au>
2012-07-03 14:49:33 +1000

    redirect /home to / instead of sending 200

--- src/main/java/org/oztrack/controller/HomeController.java
+++ src/main/java/org/oztrack/controller/HomeController.java
@@ -1,12 +1,16 @@
 package org.oztrack.controller;
 
+import java.io.IOException;
+
 import org.oztrack.data.access.SettingsDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class HomeController {
@@ -18,9 +22,18 @@ public class HomeController {
         return settingsDao.getSettings().getHomeText();
     }
 
-    @RequestMapping(value={"/", "/home"}, method=RequestMethod.GET)
+    @RequestMapping(value="/home", method=RequestMethod.GET)
+    public RedirectView redirectOldHomeUrl() throws IOException {
+        RedirectView redirectView = new RedirectView("/", true);
+        redirectView.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
+        redirectView.setExposeModelAttributes(false);
+        redirectView.setExposePathVariables(false);
+        return redirectView;
+    }
+    
+    @RequestMapping(value="/", method=RequestMethod.GET)
     @PreAuthorize("permitAll")
-    public String handleRequest() {
+    public String getHomeView() {
         return "home";
     }
 }
