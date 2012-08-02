9f2868840474e8f5ee1b2aa3ee4704f71a63baee
Charles Brooking <c.brooking@uq.edu.au>
2012-08-02 15:08:58 +1000

    fix postgres 8.x compatibility in migration 16

--- src/main/resources/db/migration/V16__project_user_id.sql
+++ src/main/resources/db/migration/V16__project_user_id.sql
@@ -1,3 +1,3 @@
 alter table project_user drop constraint project_user_pkey;
-create unique index on project_user (project_id, user_id);
+create unique index project_user_project_id_user_id_idx on project_user (project_id, user_id);
 alter table project_user add column id bigserial primary key;
