1d84326523df64067d60e342559ce159b81c3a5a
Charles Brooking <c.brooking@uq.edu.au>
2012-08-09 17:30:51 +1000

    other layers to image; style start/end using filter

--- src/main/java/org/oztrack/controller/ProjectImageController.java
+++ src/main/java/org/oztrack/controller/ProjectImageController.java
@@ -2,36 +2,54 @@ package org.oztrack.controller;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.map.DefaultMapContext;
 import org.geotools.map.FeatureLayer;
 import org.geotools.map.MapContext;
 import org.geotools.referencing.CRS;
+import org.geotools.styling.FeatureTypeStyle;
+import org.geotools.styling.Graphic;
+import org.geotools.styling.LineSymbolizer;
+import org.geotools.styling.Rule;
+import org.geotools.styling.Stroke;
+import org.geotools.styling.Style;
+import org.geotools.styling.StyleFactory;
+import org.geotools.styling.Symbolizer;
+import org.opengis.filter.FilterFactory;
+import org.opengis.style.GraphicalSymbol;
 import org.oztrack.data.access.PositionFixDao;
 import org.oztrack.data.access.ProjectDao;
 import org.oztrack.data.model.PositionFix;
 import org.oztrack.data.model.Project;
 import org.oztrack.data.model.SearchQuery;
+import org.oztrack.data.model.types.MapQueryType;
 import org.oztrack.util.MapUtils;
 import org.oztrack.view.AnimalDetectionsFeatureBuilder;
+import org.oztrack.view.AnimalStartEndFeatureBuilder;
+import org.oztrack.view.AnimalTrajectoryFeatureBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProjectImageController {
+    private final StyleFactory styleFactory = CommonFactoryFinder.getStyleFactory(null);
+    private final FilterFactory filterFactory = CommonFactoryFinder.getFilterFactory(null);
+    
     @Autowired
     private ProjectDao projectDao;
     
@@ -50,20 +68,117 @@ public class ProjectImageController {
     
     @RequestMapping(value="/projects/{id}/image", method=RequestMethod.GET, produces="image/png")
     @PreAuthorize("#project.global or hasPermission(#project, 'read')")
-    public void getView(HttpServletResponse response, Model model, @ModelAttribute(value="project") Project project) throws Exception {
-        ReferencedEnvelope mapBounds = new ReferencedEnvelope(project.getBoundingBox().getEnvelopeInternal(), CRS.decode("EPSG:" + 4326));
-        Dimension mapDimension = MapUtils.calculateMapDimension(mapBounds, 600);
-        
+    public void getView(
+        @ModelAttribute(value="project") Project project,
+        @RequestParam(value="mapQueryType", required=false) List<String> mapQueryTypes,
+        HttpServletResponse response
+    ) throws Exception {
         SearchQuery searchQuery = new SearchQuery();
         searchQuery.setProject(project);
-        List<PositionFix> positionFixList = positionFixDao.getProjectPositionFixList(searchQuery);
-        SimpleFeatureCollection featureCollection = new AnimalDetectionsFeatureBuilder(positionFixList).buildFeatureCollection();
-        FeatureLayer featureLayer = new FeatureLayer(featureCollection, MapUtils.buildPointStyle("Cross", 8, new Color(0x6Db3a7), 0.8));
-
+        ReferencedEnvelope mapBounds = new ReferencedEnvelope(project.getBoundingBox().getEnvelopeInternal(), CRS.decode("EPSG:" + 4326));
+        Dimension mapDimension = MapUtils.calculateMapDimension(mapBounds, 600);
         MapContext mapContext = new DefaultMapContext();
         mapContext.setAreaOfInterest(mapBounds);
-        mapContext.addLayer(featureLayer);
+        for (String mapQueryTypeString : mapQueryTypes) {
+            MapQueryType mapQueryType = MapQueryType.valueOf(mapQueryTypeString);
+            SimpleFeatureCollection featureCollection = null;
+            Style style = null;
+            switch (mapQueryType) {
+                case POINTS: {
+                    List<PositionFix> positionFixList = positionFixDao.getProjectPositionFixList(searchQuery);
+                    featureCollection = new AnimalDetectionsFeatureBuilder(positionFixList).buildFeatureCollection();
+                    style = buildDetectionsStyle();
+                    break;
+                }
+                case LINES: {
+                    List<PositionFix> positionFixList = positionFixDao.getProjectPositionFixList(searchQuery);
+                    featureCollection = new AnimalTrajectoryFeatureBuilder(positionFixList).buildFeatureCollection();
+                    style = buildTrajectoryStyle();
+                    break;
+                }
+                case START_END: {
+                    List<PositionFix> positionFixList = positionFixDao.getProjectPositionFixList(searchQuery);
+                    featureCollection = new AnimalStartEndFeatureBuilder(positionFixList).buildFeatureCollection();
+                    style = buildStartEndStyle();
+                    break;
+                }
+                default:
+                    throw new RuntimeException("Unsupported map query type: " + searchQuery.getMapQueryType());
+            }
+            FeatureLayer featureLayer = new FeatureLayer(featureCollection, style);
+            mapContext.addLayer(featureLayer);
+        }
         MapUtils.writePng(mapContext, mapDimension, response.getOutputStream());
         mapContext.dispose();
+    }
+
+    private Style buildDetectionsStyle() {
+        GraphicalSymbol mark = styleFactory.mark(
+            filterFactory.literal("Cross"),
+            styleFactory.createFill(filterFactory.literal(new Color(0x6Db3a7)), filterFactory.literal(0.8)),
+            null
+        );
+    
+        Graphic graphic = styleFactory.graphic(
+            Arrays.asList(mark),
+            filterFactory.literal(1.0),
+            filterFactory.literal(8),
+            filterFactory.literal(0),
+            styleFactory.anchorPoint(filterFactory.literal(0), filterFactory.literal(0)),
+            styleFactory.displacement(filterFactory.literal(0), filterFactory.literal(0))
+        );
+    
+        Symbolizer symbolizer = styleFactory.createPointSymbolizer(graphic, null);
+        Rule rule = styleFactory.createRule();
+        rule.symbolizers().add(symbolizer);
+        FeatureTypeStyle featureTypeStyle = styleFactory.createFeatureTypeStyle(new Rule[] {rule});
+        Style style = styleFactory.createStyle();
+        style.featureTypeStyles().add(featureTypeStyle);
+        return style;
+    }
+    
+    private Style buildTrajectoryStyle() {
+        Stroke stroke = styleFactory.createStroke(filterFactory.literal(new Color(0x6Db3a7)), filterFactory.literal(1.0), filterFactory.literal(1.0));
+    
+        LineSymbolizer symbolizer = styleFactory.createLineSymbolizer(stroke, null);
+        Rule rule = styleFactory.createRule();
+        rule.symbolizers().add(symbolizer);
+        FeatureTypeStyle featureTypeStyle = styleFactory.createFeatureTypeStyle(new Rule[] {rule});
+        Style style = styleFactory.createStyle();
+        style.featureTypeStyles().add(featureTypeStyle);
+        return style;
+    }
+
+    private Style buildStartEndStyle() {
+        FeatureTypeStyle featureTypeStyle = styleFactory.createFeatureTypeStyle(new Rule[] {
+            buildStartEndRule("start", new Color(0x00CD00)),
+            buildStartEndRule("end", new Color(0xCD0000))
+        });
+        Style style = styleFactory.createStyle();
+        style.featureTypeStyles().add(featureTypeStyle);
+        return style;
+    }
+
+    private Rule buildStartEndRule(String identifier, Color color) {
+        GraphicalSymbol mark = styleFactory.mark(
+            filterFactory.literal("Circle"),
+            null,
+            styleFactory.createStroke(filterFactory.literal(color), filterFactory.literal(1.2), filterFactory.literal(1.0))
+        );
+    
+        Graphic graphic = styleFactory.graphic(
+            Arrays.asList(mark),
+            filterFactory.literal(1.0),
+            filterFactory.literal(4),
+            filterFactory.literal(0),
+            styleFactory.anchorPoint(filterFactory.literal(0), filterFactory.literal(0)),
+            styleFactory.displacement(filterFactory.literal(0), filterFactory.literal(0))
+        );
+    
+        Symbolizer symbolizer = styleFactory.createPointSymbolizer(graphic, null);
+        Rule rule = styleFactory.createRule();
+        rule.setFilter(filterFactory.equals(filterFactory.property("identifier"), filterFactory.literal(identifier)));
+        rule.symbolizers().add(symbolizer);
+        return rule;
     }
 }
--- src/main/java/org/oztrack/util/MapUtils.java
+++ src/main/java/org/oztrack/util/MapUtils.java
@@ -1,58 +1,19 @@
 package org.oztrack.util;
 
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Arrays;
 
 import javax.imageio.ImageIO;
 
-import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.map.MapContext;
 import org.geotools.renderer.GTRenderer;
 import org.geotools.renderer.lite.StreamingRenderer;
-import org.geotools.styling.FeatureTypeStyle;
-import org.geotools.styling.Graphic;
-import org.geotools.styling.Rule;
-import org.geotools.styling.Style;
-import org.geotools.styling.StyleFactory;
-import org.geotools.styling.Symbolizer;
-import org.opengis.filter.FilterFactory;
 import org.opengis.geometry.Envelope;
-import org.opengis.style.GraphicalSymbol;
 
 public class MapUtils {
-    public static Style buildPointStyle(String markName, int size, Color color, double opacity) {
-        StyleFactory styleFactory = CommonFactoryFinder.getStyleFactory(null);
-        FilterFactory filterFactory = CommonFactoryFinder.getFilterFactory(null);
-    
-        GraphicalSymbol mark = styleFactory.mark(
-            filterFactory.literal(markName),
-            styleFactory.createFill(filterFactory.literal(color), filterFactory.literal(opacity)),
-            null
-        );
-    
-        Graphic graphic = styleFactory.graphic(
-            Arrays.asList(mark),
-            filterFactory.literal(1.0),
-            filterFactory.literal(size),
-            filterFactory.literal(0),
-            styleFactory.anchorPoint(filterFactory.literal(0), filterFactory.literal(0)),
-            styleFactory.displacement(filterFactory.literal(0), filterFactory.literal(0))
-        );
-    
-        Symbolizer symbolizer = styleFactory.createPointSymbolizer(graphic, null);
-        Rule rule = styleFactory.createRule();
-        rule.symbolizers().add(symbolizer);
-        FeatureTypeStyle featureTypeStyle = styleFactory.createFeatureTypeStyle(new Rule[] {rule});
-        Style style = styleFactory.createStyle();
-        style.featureTypeStyles().add(featureTypeStyle);
-        return style;
-    }
-
     public static Dimension calculateMapDimension(Envelope bounds, int width) {
         return new Dimension(width, (int) Math.round(width * (bounds.getSpan(1) / bounds.getSpan(0))));
     }
