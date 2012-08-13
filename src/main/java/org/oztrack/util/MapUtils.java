0dc637d66124368cc4cb7839b30af7ccbd3761d8
Charles Brooking <c.brooking@uq.edu.au>
2012-08-13 16:54:08 +1000

    add antialiasing to map png

--- src/main/java/org/oztrack/util/MapUtils.java
+++ src/main/java/org/oztrack/util/MapUtils.java
@@ -2,9 +2,12 @@ package org.oztrack.util;
 
 import java.awt.Dimension;
 import java.awt.Rectangle;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 
@@ -20,7 +23,11 @@ public class MapUtils {
 
     public static void writePng(MapContext mapContext, Dimension mapDimension, OutputStream out) throws IOException { 
         BufferedImage image = new BufferedImage(mapDimension.width, mapDimension.height, BufferedImage.TYPE_4BYTE_ABGR);
+        Map<RenderingHints.Key, Object> renderingHintsMap = new HashMap<RenderingHints.Key, Object>();
+        renderingHintsMap.put(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        RenderingHints hints = new RenderingHints(renderingHintsMap);
         GTRenderer renderer = new StreamingRenderer();
+        renderer.setJava2DHints(hints);
         renderer.setContext(mapContext);
         renderer.paint(image.createGraphics(), new Rectangle(mapDimension), mapContext.getAreaOfInterest());
         ImageIO.write(image, "png", out);
