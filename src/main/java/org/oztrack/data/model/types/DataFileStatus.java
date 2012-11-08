ad2627d5ea39244dbf47caaf133e368aa112512b
Charles Brooking <c.brooking@uq.edu.au>
2012-11-08 13:27:24 +1000

    remove unused classes

--- src/main/java/org/oztrack/data/model/types/DataFileStatus.java
+++ src/main/java/org/oztrack/data/model/types/DataFileStatus.java
@@ -1,12 +1,5 @@
 package org.oztrack.data.model.types;
 
-/**
- * Created by IntelliJ IDEA.
- * User: uqpnewm5
- * Date: 21/04/11
- * Time: 8:47 AM
- * To change this template use File | Settings | File Templates.
- */
 public enum DataFileStatus {
     NEW,
     PROCESSING,
--- src/main/java/org/oztrack/data/model/types/DataFileType.java
+++
@@ -1,5 +0,0 @@
-package org.oztrack.data.model.types;
-
-public enum DataFileType {
-    POSITION_FIX
-}
