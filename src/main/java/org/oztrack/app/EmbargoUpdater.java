ad3fe78c2fb33545b5f04a2dfe63e948ca7f90b2
Charles Brooking <c.brooking@uq.edu.au>
2013-01-17 11:47:59 +1000

    merge embargo updater/notifier

--- src/main/java/org/oztrack/app/EmbargoUpdater.java
+++ src/main/java/org/oztrack/app/EmbargoUpdater.java
@@ -1,7 +1,9 @@
 package org.oztrack.app;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -9,6 +11,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceUnit;
 
+import org.apache.commons.lang3.time.DateUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.oztrack.data.access.impl.ProjectDaoImpl;
@@ -31,13 +34,17 @@ public class EmbargoUpdater implements Runnable {
 
     @Override
     public void run() {
-        logger.info("Running embargo updater.");
-
+        logger.debug("Running embargo updater.");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         ProjectDaoImpl projectDao = new ProjectDaoImpl();
         projectDao.setEntityManger(entityManager);
+        Date currentDate = new Date();
+        endEmbargo(entityManager, projectDao, currentDate);
+        sendNotifications(entityManager, projectDao, currentDate, Calendar.MONTH, 2);
+    }
 
-        List<Project> projects = projectDao.getProjectsWithExpiredEmbargo(new Date());
+    private void endEmbargo(EntityManager entityManager, ProjectDaoImpl projectDao, Date currentDate) {
+        List<Project> projects = projectDao.getProjectsWithExpiredEmbargo(currentDate);
         for (Project project : projects) {
             logger.info(
                 "Making project " + project.getId() + " open access " +
@@ -66,6 +73,62 @@ public class EmbargoUpdater implements Runnable {
             }
             catch (Exception e) {
                 logger.error("Exception in embargo updater", e);
+                try {transaction.rollback();} catch (Exception e2) {}
+            }
+        }
+    }
+
+    // Note: successive calls to this method should be performed with earlier expiry dates first.
+    // See code below that skips notification for a project if one already sent for an equal or shorter period.
+    private void sendNotifications(EntityManager entityManager, ProjectDaoImpl projectDao, Date currentDate, int field, int amount) {
+        Calendar expiryCalendar = new GregorianCalendar();
+        expiryCalendar.setTime(currentDate);
+        expiryCalendar.add(field, amount);
+        Date expiryDate = DateUtils.truncate(expiryCalendar.getTime(), Calendar.DATE);
+        List<Project> projects = projectDao.getProjectsWithExpiredEmbargo(expiryDate);
+
+        for (Project project : projects) {
+            // Don't send notifications for projects already at end of embargo period.
+            // These should be picked up by the endEmargo updater, which sends its own notification.
+            if (!currentDate.before(project.getEmbargoDate())) {
+                continue;
+            }
+            // If notification has been sent for an earlier or equal expiry date, e.g. a one-week
+            // notification has been sent and we're preparing to send two-month notifications here,
+            // then skip this project. It doesn't make sense to send both if we are already in the
+            // shorter period before expiry due to the scheduler not being run for a while - or to
+            // send duplicate notifications for the same date.
+            if ((project.getEmbargoNotificationDate() != null) && !expiryDate.before(project.getEmbargoNotificationDate())) {
+                continue;
+            }
+            logger.info(
+                "Sending notification for project " + project.getId() + " " +
+                "(embargo expires " + isoDateFormat.format(project.getEmbargoDate()) + ")."
+            );
+            EntityTransaction transaction = entityManager.getTransaction();
+            transaction.begin();
+            try {
+                EmailBuilder emailBuilder = new EmailBuilder();
+                emailBuilder.to(project.getCreateUser());
+                emailBuilder.subject("OzTrack project embargo ending");
+                StringBuilder htmlMsgContent = new StringBuilder();
+                htmlMsgContent.append("<p>");
+                htmlMsgContent.append("    Please note that your OzTrack project,\n");
+                htmlMsgContent.append("    <b>" + project.getTitle() + "</b>, will end its embargo period on ");
+                htmlMsgContent.append("    " + isoDateFormat.format(project.getEmbargoDate()) + ".\n");
+                htmlMsgContent.append("</p>\n");
+                htmlMsgContent.append("<p>");
+                htmlMsgContent.append("    Starting from this date, data in this project will be made publicly available via OzTrack.");
+                htmlMsgContent.append("</p>");
+                emailBuilder.htmlMsgContent(htmlMsgContent.toString());
+                emailBuilder.build().send();
+
+                project.setEmbargoNotificationDate(expiryDate);
+                projectDao.update(project);
+                transaction.commit();
+            }
+            catch (Exception e) {
+                logger.error("Exception in embargo notifier", e);
                 try {transaction.rollback();} catch (Exception e2) {}
             }
         }
--- src/main/java/org/oztrack/controller/EmbargoController.java
+++ src/main/java/org/oztrack/controller/EmbargoController.java
@@ -1,21 +1,16 @@
 package org.oztrack.controller;
 
-import org.oztrack.app.EmbargoNotifier;
 import org.oztrack.app.EmbargoUpdater;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class EmbargoController {
     @Autowired
     private EmbargoUpdater embargoUpdater;
-
-    @Autowired
-    private EmbargoNotifier embargoNotifier;
 
     @RequestMapping(value="/settings/embargo", method=RequestMethod.GET)
     @PreAuthorize("hasRole('ROLE_ADMIN')")
@@ -25,13 +20,8 @@ public class EmbargoController {
 
     @RequestMapping(value="/settings/embargo", method=RequestMethod.POST)
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public String processSubmit(@RequestParam("action") String action) throws Exception {
-        if (action.equals("update")) {
-            embargoUpdater.run();
-        }
-        else if (action.equals("notify")) {
-            embargoNotifier.run();
-        }
+    public String processSubmit() throws Exception {
+        embargoUpdater.run();
         return "embargo-form";
     }
 }
