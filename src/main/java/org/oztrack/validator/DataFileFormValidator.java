de879617fb8856b67601e431e670bd2715a7c8f8
Charles Brooking <c.brooking@uq.edu.au>
2013-01-31 11:30:39 +1000

    make datafile description optional

--- src/main/java/org/oztrack/validator/DataFileFormValidator.java
+++ src/main/java/org/oztrack/validator/DataFileFormValidator.java
@@ -2,7 +2,6 @@ package org.oztrack.validator;
 
 import org.oztrack.data.model.DataFile;
 import org.springframework.validation.Errors;
-import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 public class DataFileFormValidator  implements Validator {
@@ -11,10 +10,10 @@ public class DataFileFormValidator  implements Validator {
         return DataFile.class.isAssignableFrom(clazz);
     }
 
+    @Override
     public void validate(Object obj, Errors errors) {
         DataFile dataFile = (DataFile) obj;
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "fileDescription", "error.empty.field", "Please enter file description");
-        if  (dataFile.getFile().getSize() == 0) {
+        if (dataFile.getFile().getSize() == 0) {
             errors.rejectValue("file", "no.file", "Please select a file");
         }
     }
