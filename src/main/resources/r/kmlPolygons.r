a651df5d513914d6a701dff9737ba21c0eb5950f
Charles Brooking <c.brooking@uq.edu.au>
2013-02-08 17:47:02 +1000

    dynamic field names and types in R to KML

--- src/main/resources/r/kmlPolygons.r
+++ src/main/resources/r/kmlPolygons.r
@@ -1,16 +1,33 @@
 ### This function is an edited version of the kmlPolygon() function in package maptools
 ### Functionality has been extended so that details from the @data slot in the SPDF are returned in the kml
 
-fOZkmlHeader <- function(folderName) {
-  c("<?xml version=\"1.0\" encoding=\"UTF-8\"?>", 
-    "<kml xmlns=\"http://www.opengis.net/kml/2.2\">", 
-    "<Document>",
-    paste('<Schema name="', 'OzTrack', '" id="OzTrackPolygons">', sep=''),
-    paste('<SimpleField name="id" type="string"/>'),
-    paste('<SimpleField name="area" type="float"/>'),
-    "</Schema>",
-    "<Folder>",
-    paste('<name>',folderName,'</name>',sep = ""))
+fOZkmlHeader <- function(folderName, OzSPDF) {
+  headers <- c("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
+  headers <- append(headers, "<kml xmlns=\"http://www.opengis.net/kml/2.2\">")
+  headers <- append(headers, "<Document>")
+  headers <- append(headers, paste('<Schema name="', 'OzTrack', '" id="OzTrackPolygons">', sep=''))
+  for (fieldName in names(as(OzSPDF, "data.frame"))) {
+    rClassName <- class(as(OzSPDF, "data.frame")[[fieldName]])
+    if ((rClassName == 'numeric') || (rClassName == 'double')) {
+      kmlFieldType <- 'double'
+    }
+    else if (rClassName == 'float') {
+      kmlFieldType <- 'float'
+    }
+    else if (rClassName == 'integer') {
+      kmlFieldType <- 'int'
+    }
+    else if (rClassName == 'logical') {
+      kmlFieldType <- 'bool'
+    }
+    else {
+      kmlFieldType <- 'string'
+    }
+    headers <- append(headers, paste('<SimpleField name="', fieldName, '" type="', kmlFieldType, '"/>', sep=''))
+  }
+  headers <- append(headers, "</Schema>")
+  headers <- append(headers, "<Folder>")
+  headers <- append(headers, paste('<name>',folderName,'</name>',sep = ""))
 }
 
 fOZkmlFooter <- function() {
@@ -19,15 +36,15 @@ fOZkmlFooter <- function() {
     "</kml>")
 }
 
-fOZkmlPlacemark <- function (obj, name, visibility=TRUE, id="", area= "", other="") {
+fOZkmlPlacemark <- function (obj, name, visibility=TRUE, id, fieldNames, fieldValues) {
   if (class(obj) != "Polygons" && class(obj) != "SpatialPolygonsDataFrame") {
     stop("obj must be of class 'Polygons' or 'SpatialPolygonsDataFrame' [package 'sp']")
   } 
   if (class(obj) == "SpatialPolygonsDataFrame") {
     if (length(obj@polygons) > 1L) {
       warning(paste("Only the first Polygons object with the ID '", 
-                    obj@polygons[[1]]@ID, "' is taken from 'obj'", 
-                    sep = ""))
+          obj@polygons[[1]]@ID, "' is taken from 'obj'", 
+          sep = ""))
     }
     var3 <- names(obj)[3]
     obj <- obj@polygons[[1]]
@@ -38,14 +55,15 @@ fOZkmlPlacemark <- function (obj, name, visibility=TRUE, id="", area= "", other=
   kml <- append(kml, paste("<visibility>", as.integer(visibility),"</visibility>", sep=""))
   kml <- append(kml, paste('<ExtendedData>'))
   kml <- append(kml, '<SchemaData schemaUrl="#OzTrackPolygons">')
-  kml <- append(kml, paste('<SimpleData name="id">',id,'</SimpleData>',sep=""))
-  kml <- append(kml, paste('<SimpleData name="area">',area,'</SimpleData>',sep=""))
-  if(is.na(var3) == FALSE)
-    if(var3 == "hval")
-      kml <- append(kml, paste('<SimpleData name="hval">',other,'</SimpleData>',sep=""))
-  if(is.na(var3) == FALSE)
-    if(var3 == "alpha")
-      kml <- append(kml, paste('<SimpleData name="alpha">',other,'</SimpleData>',sep=""))
+  for (i in 1:length(fieldNames)) {
+    if (class(fieldValues[i]) == 'logical') {
+      kmlFieldValue <- ifelse(fieldValues[i], 1, 0)
+    }
+    else {
+      kmlFieldValue <- fieldValues[i]
+    }
+    kml <- append(kml, paste('<SimpleData name="', fieldNames[i], '">', as.character(kmlFieldValue), '</SimpleData>',sep=""))
+  }
   kml <- append(kml, "</SchemaData>")
   kml <- append(kml, "</ExtendedData>")
   kml <- append(kml, "<MultiGeometry>")
@@ -72,16 +90,15 @@ fOZkmlPlacemark <- function (obj, name, visibility=TRUE, id="", area= "", other=
 fOZkmlPolygons <- function(OzSPDF, kmlFileName, folderName) {
   kmlFile <- file(kmlFileName, "w")
   kmlPlacemarks <- sapply(1:nrow(OzSPDF), function(x) {
-    lother <- ifelse(ncol(OzSPDF) > 2, as.character(as(OzSPDF, "data.frame")[x,3]), NA)
-    fOZkmlPlacemark(OzSPDF[x,],
-                  name=as.character(as(OzSPDF, "data.frame")[x,'id']), 
-                  visibility=TRUE,
-                  id=as.character(as(OzSPDF, "data.frame")[x,'id']),
-                  area=as.character(as(OzSPDF, "data.frame")[x,'area']),
-                  other=lother)
-  })
+      fOZkmlPlacemark(OzSPDF[x,],
+        name=as.character(as(OzSPDF, "data.frame")[x,'id']), 
+        visibility=TRUE,
+        fieldNames=names(as(OzSPDF, "data.frame")),
+        fieldValues=as(OzSPDF, "data.frame")[x,]
+      )
+    })
   
-  cat(fOZkmlHeader(folderName=folderName), file=kmlFile, sep="\n")
+  cat(fOZkmlHeader(folderName=folderName, OzSPDF=OzSPDF), file=kmlFile, sep="\n")
   cat(unlist(kmlPlacemarks), file=kmlFile, sep="\n")
   cat(fOZkmlFooter(), file=kmlFile, sep="\n")
   close(kmlFile)
