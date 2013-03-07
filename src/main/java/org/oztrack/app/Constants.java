bbbb5f72d5d9d400a3b8c63e2362e45956343a85
Charles Brooking <c.brooking@uq.edu.au>
2013-03-07 16:53:57 +1000

    remove unused constant

--- src/main/java/org/oztrack/app/Constants.java
+++ src/main/java/org/oztrack/app/Constants.java
@@ -1,7 +1,6 @@
 package org.oztrack.app;
 
 public interface Constants {
-    String CURRENT_USER_ID = "currentUserId";
     String namespacePrefix = "oztrack";
     String namespaceURI = "http://oztrack.org/xmlns#";
 }
