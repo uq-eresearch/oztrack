862346139cdc2d594e489da66a0bb3e6fc2de9aa
Charles Brooking <c.brooking@uq.edu.au>
2013-03-28 13:54:56 +1000

    make cc licence links consistent

---
+++ src/main/resources/db/migration/V57__data_licence_urls.sql
@@ -0,0 +1,8 @@
+update data_licence set info_url = 'http://creativecommons.org/licenses/by/3.0/'       where identifier = 'CC-BY';
+update data_licence set info_url = 'http://creativecommons.org/licenses/by-sa/3.0/'    where identifier = 'CC-BY-SA';
+update data_licence set info_url = 'http://creativecommons.org/licenses/by-nd/3.0/'    where identifier = 'CC-BY-ND';
+update data_licence set info_url = 'http://creativecommons.org/licenses/by-nc/3.0/'    where identifier = 'CC-BY-NC';
+update data_licence set info_url = 'http://creativecommons.org/licenses/by-nc-sa/3.0/' where identifier = 'CC-BY-NC-SA';
+update data_licence set info_url = 'http://creativecommons.org/licenses/by-nc-nd/3.0/' where identifier = 'CC-BY-NC-ND';
+update data_licence set info_url = 'http://creativecommons.org/publicdomain/zero/1.0/' where identifier = 'CC0';
+update data_licence set info_url = 'http://creativecommons.org/publicdomain/mark/1.0/' where identifier = 'PDM';
