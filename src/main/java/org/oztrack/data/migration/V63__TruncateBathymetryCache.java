5f3cd42d8150e5e80caa978619dcc31903006e90
Charles Brooking <c.brooking@uq.edu.au>
2013-04-17 14:32:56 +1000

    disable migration 63

--- src/main/java/org/oztrack/data/migration/V63__TruncateBathymetryCache.java
+++ src/main/java/org/oztrack/data/migration/V63__TruncateBathymetryCache.java
@@ -1,53 +1,17 @@
 package org.oztrack.data.migration;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Properties;
-
-import org.oztrack.geoserver.GeoServerClient;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.googlecode.flyway.core.migration.java.JavaMigration;
 
 public class V63__TruncateBathymetryCache implements JavaMigration {
-    private final String propertiesFilePath = "conf/properties/application.properties";
-
     @Override
     public void migrate(JdbcTemplate jdbcTemplate) throws Exception {
-        // This code has the potential to be duplicated in future migrations.
-        // Refactoring should be done where appropriate for property lookup and GeoServer client code.
-
-        Properties fileProperties = new Properties();
-        InputStream propertiesStream = this.getClass().getClassLoader().getResourceAsStream(propertiesFilePath);
-        fileProperties.load(new InputStreamReader(propertiesStream, "UTF-8"));
-
-        String username = getProperty(fileProperties, "application.geoServerUsername");
-        String password = getProperty(fileProperties, "application.geoServerPassword");
-        String baseUrl = getProperty(fileProperties, "application.geoServerLocalUrl");
-        String templateBasePath = "/geoserver";
-
-        GeoServerClient client = new GeoServerClient(username, password, baseUrl, templateBasePath);
-        for (String gridSetId : new String[] {"EPSG:4326", "EPSG:900913"}) {
-            for (String imageFormat : new String[] {"image/png", "image/jpeg"}) {
-                for (String styleName : new String[] {"oztrack_bathymetry", null}) {
-                    client
-                        .seed("oztrack:gebco_08")
-                        .template("gwc/seed.xml.ftl")
-                        .param("layerName", "oztrack:gebco_08")
-                        .param("requestType", "truncate")
-                        .param("gridSetId", gridSetId)
-                        .param("zoomStart", "00")
-                        .param("zoomStop", "20")
-                        .param("imageFormat", imageFormat)
-                        .param("styleName", styleName)
-                        .param("threadCount", 1)
-                        .post();
-                }
-            }
-        }
-    }
-
-    private String getProperty(Properties fileProperties, String key) {
-        return System.getProperty(key, fileProperties.getProperty(key));
+        // This migration had code that used GeoServerClient to empty a GeoWebCache layer.
+        // However, it caused issues in the production environment where both the GeoServer
+        // and OzTrack webapps run in the same Tomcat instance: OzTrack started first and
+        // tried to make REST API requests to GeoServer. Need to be smarter about waiting
+        // for GeoServer to start before deciding that a request has failed. Disabling this
+        // migration for now until we solve this problem. See git history for old code.
     }
 }
