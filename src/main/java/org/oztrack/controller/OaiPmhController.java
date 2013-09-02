9a4069045c05dc21c8c7dd06dd07ff395ebf5ad2
Charles Brooking <c.brooking@uq.edu.au>
2013-09-02 14:04:07 +1000

    oai-pmh: UTC timezone in response; error conds

--- src/main/java/org/oztrack/controller/OaiPmhController.java
+++ src/main/java/org/oztrack/controller/OaiPmhController.java
@@ -3,17 +3,17 @@ package org.oztrack.controller;
 import java.io.PrintWriter;
 import java.util.GregorianCalendar;
 import java.util.Map;
+import java.util.TimeZone;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringEscapeUtils;
-import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang3.time.FastDateFormat;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.AbstractView;
 
@@ -21,9 +21,11 @@ import org.springframework.web.servlet.view.AbstractView;
 public class OaiPmhController {
     private static class OaiPmhErrorView extends AbstractView {
         private final String code;
+        private final String message;
 
-        public OaiPmhErrorView(String code) {
+        public OaiPmhErrorView(String code, String message) {
             this.code = code;
+            this.message = message;
         }
 
         @Override
@@ -32,45 +34,77 @@ public class OaiPmhController {
             HttpServletRequest request,
             HttpServletResponse response
         ) throws Exception {
-            String responseDate = DateFormatUtils.ISO_DATETIME_TIME_ZONE_FORMAT.format(new GregorianCalendar());
-            String requestURL = request.getRequestURL().toString();
+            FastDateFormat utcDateTimeFormat = FastDateFormat.getInstance("yyyy-MM-dd'T'HH:mm:ss'Z'");
+            GregorianCalendar currentUtcDateTime = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
             PrintWriter out = response.getWriter();
             out.append("<OAI-PMH\n");
             out.append("  xmlns=\"http://www.openarchives.org/OAI/2.0/\"\n");
             out.append("  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n");
             out.append("  xsi:schemaLocation=\"http://www.openarchives.org/OAI/2.0/ http://www.openarchives.org/OAI/2.0/OAI-PMH.xsd\">\n");
-            out.append("  <responseDate>" + StringEscapeUtils.escapeXml(responseDate) + "</responseDate>\n");
-            out.append("  <request>" + StringEscapeUtils.escapeXml(requestURL) + "</request>\n");
-            out.append("  <error code=\"" + StringEscapeUtils.escapeXml(code) + "\" />\n");
+            out.append("  <responseDate>" + StringEscapeUtils.escapeXml(utcDateTimeFormat.format(currentUtcDateTime)) + "</responseDate>\n");
+            out.append("  <request>" + StringEscapeUtils.escapeXml(request.getRequestURL().toString()) + "</request>\n");
+            out.append("  <error code=\"" + StringEscapeUtils.escapeXml(code) + "\">" + StringEscapeUtils.escapeXml(message) + "</error>\n");
             out.append("</OAI-PMH>\n");
         }
     }
 
     @RequestMapping(value="/oai", method=RequestMethod.GET)
     @PreAuthorize("permitAll")
-    public View handleRequest(
-        @RequestParam(value="verb") String verb
-    ) {
+    public View handleRequest(HttpServletRequest request) {
+        String[] verbs = request.getParameterValues("verb");
+        if (verbs == null) {
+            return new OaiPmhErrorView("badVerb", "Verb argument is missing.");
+        }
+        if (verbs.length > 1) {
+            return new OaiPmhErrorView("badVerb", "Verb argument is repeated.");
+        }
+        String verb = verbs[0];
         if (verb.equals("Identify")) {
+            // Possible errors:
+            // - badArgument
             throw new UnsupportedOperationException();
         }
         else if (verb.equals("ListMetadataFormats")) {
+            // Possible errors:
+            // - badArgument
+            // - idDoesNotExist
+            // - noMetadataFormats
             throw new UnsupportedOperationException();
         }
         else if (verb.equals("ListSets")) {
+            // Possible errors:
+            // - badArgument
+            // - badResumptionToken
+            // - noSetHierarchy
             throw new UnsupportedOperationException();
         }
         else if (verb.equals("ListIdentifiers")) {
+            // Possible errors:
+            // - badArgument
+            // - badResumptionToken
+            // - cannotDisseminateFormat
+            // - noRecordsMatch
+            // - noSetHierarchy
             throw new UnsupportedOperationException();
         }
         else if (verb.equals("ListRecords")) {
+            // Possible errors:
+            // - badArgument
+            // - badResumptionToken
+            // - cannotDisseminateFormat
+            // - noRecordsMatch
+            // - noSetHierarchy
             throw new UnsupportedOperationException();
         }
         else if (verb.equals("GetRecord")) {
+            // Possible errors:
+            // - badArgument
+            // - cannotDisseminateFormat
+            // - idDoesNotExist
             throw new UnsupportedOperationException();
         }
         else {
-            return new OaiPmhErrorView("badVerb");
+            return new OaiPmhErrorView("badVerb", "Verb argument is not a legal OAI-PMH verb.");
         }
     }
 }
