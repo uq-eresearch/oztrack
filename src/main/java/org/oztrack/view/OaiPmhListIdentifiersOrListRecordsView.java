e68aa97a17d4f1561e25591bf578cd55848a17ef
Charles Brooking <c.brooking@uq.edu.au>
2013-09-11 14:35:19 +1000

    oai-pmh: minor refactor

--- src/main/java/org/oztrack/view/OaiPmhListIdentifiersOrListRecordsView.java
+++ src/main/java/org/oztrack/view/OaiPmhListIdentifiersOrListRecordsView.java
@@ -35,16 +35,11 @@ public class OaiPmhListIdentifiersOrListRecordsView extends OaiPmhView {
     @Override
     protected void writeMainElement(XMLStreamWriter out) throws XMLStreamException {
         out.writeStartElement(verb);
-        if (metadataPrefix.equals(OAI_DC.nsPrefix)) {
-            writeOaiDcRepositoryRecordElement(out);
-        }
-        else if (metadataPrefix.equals(RIF_CS.nsPrefix)) {
-            writeRifCsRepositoryRecordElement(out);
-        }
+        writeRepositoryRecordElement(out);
         out.writeEndElement(); // verb
     }
 
-    private void writeOaiDcRepositoryRecordElement(XMLStreamWriter out) throws XMLStreamException {
+    private void writeRepositoryRecordElement(XMLStreamWriter out) throws XMLStreamException {
         out.writeStartElement("record");
         out.writeStartElement("header");
         writeSimpleElement(out, "identifier", oztrackRepositoryIdentifier);
@@ -52,105 +47,108 @@ public class OaiPmhListIdentifiersOrListRecordsView extends OaiPmhView {
         out.writeEndElement(); // header
         if (verb.equals("ListRecords")) {
             out.writeStartElement("metadata");
-            out.writeStartElement(OAI_DC.nsPrefix, "dc", OAI_DC.nsUri);
-            // Every metadata part must include xmlns attributes for its metadata formats.
-            // http://www.openarchives.org/OAI/2.0/openarchivesprotocol.htm#Record
-            out.setPrefix(OAI_DC.nsPrefix, OAI_DC.nsUri);
-            out.writeNamespace(OAI_DC.nsPrefix, OAI_DC.nsUri);
-            out.setPrefix(DC.nsPrefix, DC.nsUri);
-            out.writeNamespace(DC.nsPrefix, DC.nsUri);
-            // Every metadata part must include the attributes xmlns:xsi (namespace URI for XML schema) and
-            // xsi:schemaLocation (namespace URI and XML schema URL for validating metadata that follows).
-            // http://www.openarchives.org/OAI/2.0/openarchivesprotocol.htm#Record
-            out.setPrefix(XSI.nsPrefix, XSI.nsUri);
-            out.writeNamespace(XSI.nsPrefix, XSI.nsUri);
-            out.writeAttribute(XSI.nsUri, "schemaLocation", OAI_DC.nsUri + " " + OAI_DC.xsdUri);
-            writeSimpleElement(out, DC.nsUri, "title", oztrackRepositoryTitle);
-            writeSimpleElement(out, DC.nsUri, "description", oztrackRepositoryDescription);
-            writeSimpleElement(out, DC.nsUri, "creator", oztrackRepositoryCreator);
-            writeSimpleElement(out, DC.nsUri, "created", oztrackRepositoryCreateDate);
-            writeSimpleElement(out, DC.nsUri, "date", oztrackRepositoryUpdateDate);
-            writeSimpleElement(out, DC.nsUri, "type", "Service");
-            writeSimpleElement(out, DC.nsUri, "identifier", oztrackRepositoryIdentifier);
-            writeSimpleElement(out, DC.nsUri, "language", "english");
-            out.writeEndElement(); // dc
+            if (metadataPrefix.equals(OAI_DC.nsPrefix)) {
+                writeOaiDcRepositoryMetadataElement(out);
+            }
+            else if (metadataPrefix.equals(RIF_CS.nsPrefix)) {
+                writeRifCsRepositoryMetadataElement(out);
+            }
             out.writeEndElement(); // metadata
         }
         out.writeEndElement(); // record
     }
 
-    private void writeRifCsRepositoryRecordElement(XMLStreamWriter out) throws XMLStreamException {
-        out.writeStartElement("record");
-        out.writeStartElement("header");
-        writeSimpleElement(out, "identifier", oztrackRepositoryIdentifier);
-        writeSimpleElement(out, "datestamp", oztrackRepositoryUpdateDate);
-        out.writeEndElement(); // header
-        if (verb.equals("ListRecords")) {
-            out.writeStartElement("metadata");
-            out.writeStartElement(RIF_CS.nsPrefix, "registryObjects", RIF_CS.nsUri);
+    private void writeOaiDcRepositoryMetadataElement(XMLStreamWriter out) throws XMLStreamException {
+        out.writeStartElement(OAI_DC.nsPrefix, "dc", OAI_DC.nsUri);
 
-            // Every metadata part must include xmlns attributes for its metadata formats.
-            // http://www.openarchives.org/OAI/2.0/openarchivesprotocol.htm#Record
-            out.setPrefix(RIF_CS.nsPrefix, RIF_CS.nsUri);
-            out.writeNamespace(RIF_CS.nsPrefix, RIF_CS.nsUri);
+        // Every metadata part must include xmlns attributes for its metadata formats.
+        // http://www.openarchives.org/OAI/2.0/openarchivesprotocol.htm#Record
+        out.setPrefix(OAI_DC.nsPrefix, OAI_DC.nsUri);
+        out.writeNamespace(OAI_DC.nsPrefix, OAI_DC.nsUri);
+        out.setPrefix(DC.nsPrefix, DC.nsUri);
+        out.writeNamespace(DC.nsPrefix, DC.nsUri);
 
-            // Every metadata part must include the attributes xmlns:xsi (namespace URI for XML schema) and
-            // xsi:schemaLocation (namespace URI and XML schema URL for validating metadata that follows).
-            // http://www.openarchives.org/OAI/2.0/openarchivesprotocol.htm#Record
-            out.setPrefix(XSI.nsPrefix, XSI.nsUri);
-            out.writeNamespace(XSI.nsPrefix, XSI.nsUri);
-            out.writeAttribute(XSI.nsUri, "schemaLocation", RIF_CS.nsUri + " " + RIF_CS.xsdUri);
+        // Every metadata part must include the attributes xmlns:xsi (namespace URI for XML schema) and
+        // xsi:schemaLocation (namespace URI and XML schema URL for validating metadata that follows).
+        // http://www.openarchives.org/OAI/2.0/openarchivesprotocol.htm#Record
+        out.setPrefix(XSI.nsPrefix, XSI.nsUri);
+        out.writeNamespace(XSI.nsPrefix, XSI.nsUri);
+        out.writeAttribute(XSI.nsUri, "schemaLocation", OAI_DC.nsUri + " " + OAI_DC.xsdUri);
 
-            out.writeStartElement(RIF_CS.nsUri, "registryObject");
-            out.writeAttribute("group", oztrackRifCsGroup);
+        writeSimpleElement(out, DC.nsUri, "title", oztrackRepositoryTitle);
+        writeSimpleElement(out, DC.nsUri, "description", oztrackRepositoryDescription);
+        writeSimpleElement(out, DC.nsUri, "creator", oztrackRepositoryCreator);
+        writeSimpleElement(out, DC.nsUri, "created", oztrackRepositoryCreateDate);
+        writeSimpleElement(out, DC.nsUri, "date", oztrackRepositoryUpdateDate);
+        writeSimpleElement(out, DC.nsUri, "type", "Service");
+        writeSimpleElement(out, DC.nsUri, "identifier", oztrackRepositoryIdentifier);
+        writeSimpleElement(out, DC.nsUri, "language", "english");
 
-            // TODO: Do not use the identifier for an object as the key for a metadata record describing
-            // that object - the metadata record needs its own unique separate identifier.
-            // http://ands.org.au/guides/cpguide/cpgidentifiers.html
-            writeSimpleElement(out, RIF_CS.nsUri, "key", oztrackRepositoryIdentifier);
+        out.writeEndElement(); // dc
+    }
 
-            out.writeStartElement(RIF_CS.nsUri, "originatingSource");
-            out.writeAttribute("type", "authoritative");
-            out.writeCharacters(oztrackRepositoryUrl);
-            out.writeEndElement(); // originatingSource
+    private void writeRifCsRepositoryMetadataElement(XMLStreamWriter out) throws XMLStreamException {
+        out.writeStartElement(RIF_CS.nsPrefix, "registryObjects", RIF_CS.nsUri);
 
-            out.writeStartElement(RIF_CS.nsUri, "service");
-            out.writeAttribute("type", "report");
-            out.writeAttribute("dateModified", oztrackRepositoryUpdateDate);
+        // Every metadata part must include xmlns attributes for its metadata formats.
+        // http://www.openarchives.org/OAI/2.0/openarchivesprotocol.htm#Record
+        out.setPrefix(RIF_CS.nsPrefix, RIF_CS.nsUri);
+        out.writeNamespace(RIF_CS.nsPrefix, RIF_CS.nsUri);
 
-            out.writeStartElement(RIF_CS.nsUri, "identifier");
-            out.writeAttribute("type", "uri");
-            out.writeCharacters(oztrackRepositoryIdentifier);
-            out.writeEndElement(); // identifier
+        // Every metadata part must include the attributes xmlns:xsi (namespace URI for XML schema) and
+        // xsi:schemaLocation (namespace URI and XML schema URL for validating metadata that follows).
+        // http://www.openarchives.org/OAI/2.0/openarchivesprotocol.htm#Record
+        out.setPrefix(XSI.nsPrefix, XSI.nsUri);
+        out.writeNamespace(XSI.nsPrefix, XSI.nsUri);
+        out.writeAttribute(XSI.nsUri, "schemaLocation", RIF_CS.nsUri + " " + RIF_CS.xsdUri);
 
-            out.writeStartElement(RIF_CS.nsUri, "name");
-            out.writeAttribute("type", "primary");
-            writeSimpleElement(out, RIF_CS.nsUri, "namePart", oztrackRepositoryTitle);
-            out.writeEndElement(); // name
+        out.writeStartElement(RIF_CS.nsUri, "registryObject");
+        out.writeAttribute("group", oztrackRifCsGroup);
 
-            writeSimpleElement(out, RIF_CS.nsUri, "description", oztrackRepositoryDescription);
+        // TODO: Do not use the identifier for an object as the key for a metadata record describing
+        // that object - the metadata record needs its own unique separate identifier.
+        // http://ands.org.au/guides/cpguide/cpgidentifiers.html
+        writeSimpleElement(out, RIF_CS.nsUri, "key", oztrackRepositoryIdentifier);
 
-            out.writeStartElement(RIF_CS.nsUri, "location");
-            out.writeStartElement(RIF_CS.nsUri, "address");
-            out.writeStartElement(RIF_CS.nsUri, "electronic");
-            out.writeAttribute("type", "url");
-            writeSimpleElement(out, RIF_CS.nsUri, "value", oztrackRepositoryUrl);
-            out.writeEndElement(); // electronic
-            out.writeEndElement(); // address
-            out.writeEndElement(); // location
+        out.writeStartElement(RIF_CS.nsUri, "originatingSource");
+        out.writeAttribute("type", "authoritative");
+        out.writeCharacters(oztrackRepositoryUrl);
+        out.writeEndElement(); // originatingSource
 
-            out.writeStartElement(RIF_CS.nsUri, "existenceDates");
-            out.writeStartElement(RIF_CS.nsUri, "startDate");
-            out.writeAttribute("dateFormat", "W3CDTF");
-            out.writeCharacters(oztrackRepositoryCreateDate);
-            out.writeEndElement(); // startDate
-            out.writeEndElement(); //existenceDates
+        out.writeStartElement(RIF_CS.nsUri, "service");
+        out.writeAttribute("type", "report");
+        out.writeAttribute("dateModified", oztrackRepositoryUpdateDate);
 
-            out.writeEndElement(); // service
-            out.writeEndElement(); // registryObject
-            out.writeEndElement(); // registryObjects
-            out.writeEndElement(); // metadata
-        }
-        out.writeEndElement(); // record
+        out.writeStartElement(RIF_CS.nsUri, "identifier");
+        out.writeAttribute("type", "uri");
+        out.writeCharacters(oztrackRepositoryIdentifier);
+        out.writeEndElement(); // identifier
+
+        out.writeStartElement(RIF_CS.nsUri, "name");
+        out.writeAttribute("type", "primary");
+        writeSimpleElement(out, RIF_CS.nsUri, "namePart", oztrackRepositoryTitle);
+        out.writeEndElement(); // name
+
+        writeSimpleElement(out, RIF_CS.nsUri, "description", oztrackRepositoryDescription);
+
+        out.writeStartElement(RIF_CS.nsUri, "location");
+        out.writeStartElement(RIF_CS.nsUri, "address");
+        out.writeStartElement(RIF_CS.nsUri, "electronic");
+        out.writeAttribute("type", "url");
+        writeSimpleElement(out, RIF_CS.nsUri, "value", oztrackRepositoryUrl);
+        out.writeEndElement(); // electronic
+        out.writeEndElement(); // address
+        out.writeEndElement(); // location
+
+        out.writeStartElement(RIF_CS.nsUri, "existenceDates");
+        out.writeStartElement(RIF_CS.nsUri, "startDate");
+        out.writeAttribute("dateFormat", "W3CDTF");
+        out.writeCharacters(oztrackRepositoryCreateDate);
+        out.writeEndElement(); // startDate
+        out.writeEndElement(); //existenceDates
+
+        out.writeEndElement(); // service
+        out.writeEndElement(); // registryObject
+        out.writeEndElement(); // registryObjects
     }
 }
